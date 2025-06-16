48e24b4dea024665a9a304417c8445cd7e816cad
Sanal-T <sanalt2024@gmail.com>
2025-06-16 22:17:06 +0530

    App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import JobListPage from "./pages/JobListPage";
+
+function App() {
+  return (
+    <div className="App">
+      <JobListPage />
+    </div>
+  );
+}
+
+export default App;
